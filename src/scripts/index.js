a2b251b1fce53008dbd7039177f1d28e85c208d5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-30 19:54:22 -0300

    versao final

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+// Exercício 1
+var indefinido = undefined;
+console.log(indefinido);
+
+//output: undefined
+
+// Exercício 2
+
+var numero1 = 40;
+var numero2 = 6;
+resto = numero1 % numero2
+console.log(resto)
+
+// O resto da divisão de 40 por 6 é 4
+//output: 4
